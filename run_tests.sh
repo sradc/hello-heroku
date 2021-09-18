b5b71e8928d8c35e1fc32470376c8c88fd62f49c
sradc <sidneyradcliffe@sky.com>
2021-09-18 17:35:49 +0100

    fix to actually exit if a subtest fails

--- run_tests.sh
+++ run_tests.sh
@@ -2,22 +2,28 @@
 
 printf "\n-- Running flake8, to look for syntax errors.\n"
 bash tests/run_flake8.sh
-echo  "--" exit code $?
-if [ $? -ne 0 ]; then
+exit_code=$?
+echo  "-- exit code $exit_code"
+if [ $exit_code -ne 0 ]; then
+     echo "Test failed."
      exit 1
 fi
 
 printf "\n\n-- Checking that '.py' files are Black formatted.\n"
 bash tests/are_all_py_files_formatted.sh
-echo "--" exit code $?
-if [ $? -ne 0 ]; then
+exit_code=$?
+echo "-- exit code $exit_code"
+if [ $exit_code -ne 0 ]; then
+     echo "Test failed."
      exit 1
 fi
 
 printf "\n\n-- Running pytest tests.\n"
 python -m pytest
-echo  "--" exit code $?
-if [ $? -ne 0 ]; then
+exit_code
+echo  "-- exit code $exit_code"
+if [ $exit_code -ne 0 ]; then
+     echo "Test failed."
      exit 1
 fi
 
