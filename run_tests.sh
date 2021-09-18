9175cfde9ae364aaf6be91c2b30fdd727838cb9d
sradc-sensity <sidney@sensity.ai>
2021-09-18 15:35:51 +0100

    modify print statements in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-printf "Running flake8, to look for syntax errors.\n"
+printf "\n-- Running flake8, to look for syntax errors.\n"
 bash tests/run_flake8.sh
-echo exit code $?
+echo  "--" exit code $?
 if [ $? -eq 1 ]; then
      exit 1
 fi
 
-printf "\n\nChecking that '.py' files are Black formatted.\n"
+printf "\n\n-- Checking that '.py' files are Black formatted.\n"
 bash tests/are_all_py_files_formatted.sh
-echo exit code $?
+echo "--" exit code $?
 if [ $? -eq 1 ]; then
      exit 1
 fi
 
-printf "\n\nRunning pytest.\n"
+printf "\n\n-- Running pytest tests.\n"
 python -m pytest
-echo Pytest exited $?
-echo exit code $?
+echo  "--" exit code $?
 if [ $? -eq 1 ]; then
      exit 1
 fi
+
+printf "\n\n-- All tests have passed, :D\n"
